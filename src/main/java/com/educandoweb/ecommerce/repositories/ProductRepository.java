17c49599f7411e8ba2fcf4dec827ad5ab3bf6cc9
Débora <deboracm123@hotmail.com>
2025-12-12 10:02:30 -0300

    Criando o repositorio de product

---
+++ src/main/java/com/educandoweb/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.educandoweb.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoweb.ecommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+}
